57e8e078947708d3ad167e0bb286cd95210d8cff
bendymachine01 <harodmartinez@colegioguanenta.edu.co>
2025-02-14 07:15:08 -0500

    o

--- while_2.py
+++ while_2.py
@@ -14,8 +14,9 @@ n = 0
 # procssing:
 # calcular el número de rebotes hasta que h < q
 while h >= q:
-    h *= 0.9  # La pelota sube un 10% menos en cada rebote
-    n += 1
+      h *= 0.9
+
+      n += 1
 
 # output
 print(f"La pelota no alcanza a subir la quinta parte de la altura inicial en el rebote número {n}.")
